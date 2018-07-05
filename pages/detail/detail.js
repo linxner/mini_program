54497ddbcbff01753c50cf2e08643d6870c13d39
linxner <342709615@qq.com>
2018-07-05 18:01:04 +0800

    2

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -6,59 +6,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-    detail: [],
-    comment: []
+    detail: {},
+    comment: [],
+    rating: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    var detail = wx.getStorageSync('hot')
-    this.setData({
-      detail: detail[2]
+    // var detail = wx.getStorageSync('hot')
+    // this.setData({
+    //   detail: detail[2]
+    // })
+    var that = this
+    wx.request({
+      url: `http://120.76.205.241:8000/movie/douban?sort=T&get_all_actor=1&detail_awards=1&id=${options.id}&apikey=eftGXFi8mzKdoA4EKMhWu7IjthNeIuRUrerH7c57ZzC29oufLpfexmNcV4Hzx2DW`,
+      success(res) {
+        that.setData({
+          detail: res.data.data[0],
+          rating: 110-Math.round(res.data.data[0].rating) * 11
+        })
+      }
     })
-    // var that = this
-    // wx.request({
-    //   url: `http://120.76.205.241:8000/movie/douban?sort=T&get_all_actor=1&detail_awards=1&id=${options.id}&apikey=eftGXFi8mzKdoA4EKMhWu7IjthNeIuRUrerH7c57ZzC29oufLpfexmNcV4Hzx2DW`,
-    //   success(res) {
-    //     that.setData({
-    //       detail: res.data.data[0]
-    //     })
-    //   }
-    // })
-    // wx.request({
-    //   url: `http://120.76.205.241:8000/comment/douban?id=${options.id}&parent=movie&apikey=eftGXFi8mzKdoA4EKMhWu7IjthNeIuRUrerH7c57ZzC29oufLpfexmNcV4Hzx2DW`,
-    //   success(res) {
-    //     that.setData({
-    //       comment: res.data.data
-    //     })
-    //   }
-    // })
-    // const hot = wx.getStorageSync('hot')
-    // const episode = wx.getStorageSync('episode')
-    // if (options.name === 'api') {
-    //   wx.setNavigationBarTitle({
-    //     title: `${api.api[options.index].name}`,
-    //   })
-    //   this.setData({
-    //     detail: api.api[options.index]
-    //   })
-    // } else if (options.name === 'hot') {
-    //   wx.setNavigationBarTitle({
-    //     title: `${hot[options.index].title}`,
-    //   })
-    //   this.setData({
-    //     detail: hot[options.index]
-    //   })
-    // } else {
-    //   wx.setNavigationBarTitle({
-    //     title: `${episode[options.index].title}`,
-    //   })
-    //   this.setData({
-    //     detail: episode[options.index]
-    //   })
-    // }
+    wx.request({
+      url: `http://120.76.205.241:8000/comment/douban?id=${options.id}&parent=movie&apikey=eftGXFi8mzKdoA4EKMhWu7IjthNeIuRUrerH7c57ZzC29oufLpfexmNcV4Hzx2DW`,
+      success(res) {
+        that.setData({
+          comment: res.data.data
+        })
+      }
+    })
+    const hot = wx.getStorageSync('hot')
+    const episode = wx.getStorageSync('episode')
+    if (options.name === 'api') {
+      wx.setNavigationBarTitle({
+        title: `${api.api[options.index].name}`,
+      })
+      this.setData({
+        detail: api.api[options.index]
+      })
+    } else if (options.name === 'hot') {
+      wx.setNavigationBarTitle({
+        title: `${hot[options.index].title}`,
+      })
+      this.setData({
+        detail: hot[options.index]
+      })
+    } else {
+      wx.setNavigationBarTitle({
+        title: `${episode[options.index].title}`,
+      })
+      this.setData({
+        detail: episode[options.index]
+      })
+    }
   },
 
   /**
